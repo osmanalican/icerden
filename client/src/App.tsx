b868900efc83be25ca503be64bfb371b6a0e67a9
osman <osmanalican08@gmail.com>
2026-06-29 14:33:54 +0300

    feat: integrate tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <main>
-      <h1>İçerden</h1>
-      <p>Şirketleri içeriden tanı.</p>
+    <main className="flex min-h-screen items-center justify-center bg-slate-950 text-white">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">İçerden</h1>
+
+        <p className="mt-4 text-slate-400">
+          Şirketleri içeriden tanı.
+        </p>
+      </div>
     </main>
   );
 }
